5933ba123016b09ea99b3f66eed7a4c6d2882028
Jeff Palmer <jpalmer@weatherbill.com>
2012-11-08 00:15:05 -0800

    transition to el-get

--- elpa/starter-kit-lisp-2.0.2/starter-kit-lisp-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "starter-kit-lisp" "2.0.2" "Saner defaults and goodies for lisp languages" (quote ((starter-kit "2.0.1"))))
--- init.el
+++ init.el
@@ -1,27 +1,45 @@
-(require 'package)
-(add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/") t)
-(package-initialize)
+;;; A quick & ugly PATH solution to Emacs on Mac OSX
+(if (string-equal "darwin" (symbol-name system-type))
+    (setenv "PATH" (concat "/usr/local/bin:" (getenv "PATH"))))
 
-;;;
-;; My customizations
+;; el-get package management
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+
+(setq el-get-sources
+      '((:name exec-path-from-shell :type elpa)
+        (:name ido-ubiquitous :type elpa)
+        (:name clojure-project-mode :type elpa)
+        (:name clojure-test-mode :type elpa)))
+
+(setq el-get-packages
+      '(el-get
+        clojure-mode
+        color-theme
+        color-theme-solarized))
+
+(setq my-packages
+      (append el-get-packages
+              (mapcar 'el-get-source-name el-get-sources)))
+(el-get 'sync my-packages)
+
 ;; fix the PATH variable
-(defun set-exec-path-from-shell-PATH ()
-  (let ((path-from-shell (shell-command-to-string "$SHELL -i -c 'echo $PATH'")))
-  (setenv "PATH" path-from-shell)
-  (setq exec-path (split-string path-from-shell path-separator))))
-(if window-system (set-exec-path-from-shell-PATH))
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
 
-(let ((default-directory "~/.emacs.d/site-lisp/"))
-  (normal-top-level-add-to-load-path '("."))
-  (normal-top-level-add-subdirs-to-load-path))
+;; use true OS X fullscreen
+(global-set-key (kbd "M-RET") 'ns-toggle-fullscreen)
 
 ;; Set some window defaults
 (setq default-frame-alist
       '((width . 100) (height . 56)))
 
 ;; Use the solarized theme
-(require 'color-theme-solarized)
 (color-theme-solarized-dark)
 
 ;; I *hate* this keybinding
@@ -30,12 +48,18 @@
 ;; Enable backup files.
 (setq make-backup-files t)
 
-;; Enable versioning with default values (keep five last versions, I think!)
+;; Enable versioning with default values
 (setq version-control t)
 
 ;; Save all backup file in this directory.
 (setq backup-directory-alist (quote ((".*" . "~/.emacs_backups/"))))
 (setq delete-old-versions t)
+
+;; ido mode support
+(setq ido-enable-flex-matching t)
+(setq ido-everywhere t)
+(ido-mode 1)
+(ido-ubiquitous)
 
 ;; don't EVER put tabs in indents
 (setq-default indent-tabs-mode nil)
@@ -66,9 +90,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(inhibit-startup-screen t)
  '(kill-whole-line t)
- '(menu-bar-mode t)
- '(show-trailing-whitespace t))
+ '(menu-bar-mode nil)
+ '(show-trailing-whitespace t)
+ '(tool-bar-mode nil)
+ '(tooltip-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
