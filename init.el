64d944f202bcb730a223e2a7415e44721c446cb9
Jeff Palmer <jeff@srcstory.com>
2013-07-09 15:53:44 -0700

    moved to MELPA
    - moved to the MELPA repo
    - changed the default font
    - moved to another solarized theme

--- init.el
+++ init.el
@@ -5,7 +5,7 @@
 
 (require 'package)
 (add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/"))
+             '("melpa" . "http://melpa.milkbox.net/packages/"))
 (package-initialize)
 (when (not package-archive-contents)
   (package-refresh-contents))
@@ -14,8 +14,11 @@
                       starter-kit-lisp
                       starter-kit-bindings
                       scpaste
-                      clojure-mode clojure-test-mode
+                      clojure-mode
+                      clojure-test-mode
+                      slime
                       nrepl ac-nrepl
+                      midje-mode
                       markdown-mode
                       yaml-mode
                       puppet-mode
@@ -23,7 +26,10 @@
                       scss-mode
                       coffee-mode
                       marmalade
-                      color-theme color-theme-solarized))
+                      color-theme
+                      color-theme-solarized
+                      color-theme-sanityinc-solarized
+                      nzenburn-theme))
 
 (dolist (p my-packages)
   (when (not (package-installed-p p))
@@ -41,11 +47,9 @@
 
 ;; Set some window defaults
 (setq default-frame-alist
-      '((width . 100) (height . 56)))
+      '((width . 96) (height . 53)))
 (menu-bar-mode)
 
-;; Use the solarized theme
-(color-theme-solarized-dark)
 
 ;; I *hate* this keybinding outside of the command line
 (when window-system
@@ -78,6 +82,17 @@
 (add-hook 'text-mode-hook 'turn-on-auto-fill)
 
 ;; clojure support
+(require 'clojure-mode)
+(define-clojure-indent
+  (defroutes 'defun)
+  (GET 2)
+  (POST 2)
+  (PUT 2)
+  (DELETE 2)
+  (HEAD 2)
+  (ANY 2)
+  (context 2))
+
 ;; nrepl
 (add-hook 'nrepl-interaction-mode-hook 'nrepl-turn-on-eldoc-mode)
 (setq nrepl-popup-stacktraces nil)
@@ -113,19 +128,41 @@
 ;; invoke puppet mode for .pp files
 (add-to-list 'auto-mode-alist '("\\.pp\\'" . puppet-mode))
 
+(when (eq system-type 'darwin)
+      ;; default Latin font (e.g. Consolas)
+      (set-face-attribute 'default nil :family "Inconsolata")
+      ;; default font size (point * 10)
+      (set-face-attribute 'default nil :height 160))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ac-delay 2.0)
+ '(ansi-color-faces-vector [default bold shadow italic underline bold bold-italic bold])
+ '(ansi-color-names-vector (vector "#52676f" "#c60007" "#728a05" "#a57705" "#2075c7" "#c61b6e" "#259185" "#fcf4dc"))
+ '(background-color "#042028")
+ '(background-mode dark)
+ '(blink-cursor-mode nil)
  '(coffee-tab-width 2)
+ '(color-theme-sanityinc-solarized-rgb-is-srgb t)
+ '(cursor-color "#708183")
+ '(custom-enabled-themes (quote (sanityinc-solarized-dark)))
+ '(custom-safe-themes (quote ("4c9ba94db23a0a3dea88ee80f41d9478c151b07cb6640b33bfc38be7c2415cc4" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" default)))
+ '(desktop-save-mode t)
+ '(fci-rule-color "#e9e2cb")
+ '(foreground-color "#708183")
  '(inhibit-startup-screen t)
  '(kill-whole-line t)
  '(scss-compile-at-save nil)
  '(scss-sass-command "sass")
+ '(show-paren-mode t)
  '(show-trailing-whitespace t)
- '(tool-bar-mode nil))
+ '(tool-bar-mode nil)
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map (quote ((20 . "#c60007") (40 . "#bd3612") (60 . "#a57705") (80 . "#728a05") (100 . "#259185") (120 . "#2075c7") (140 . "#c61b6e") (160 . "#5859b7") (180 . "#c60007") (200 . "#bd3612") (220 . "#a57705") (240 . "#728a05") (260 . "#259185") (280 . "#2075c7") (300 . "#c61b6e") (320 . "#5859b7") (340 . "#c60007") (360 . "#bd3612"))))
+ '(vc-annotate-very-old-color nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
