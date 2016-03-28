8fee2eac4d678bf18b8ac1ee4cedb378214eac50
Jeff Palmer <jpalmer@comprehend.com>
2016-03-28 14:47:31 -0700

    update to the mode line

--- custom.el
+++ custom.el
@@ -94,5 +94,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- ;; '(mode-line ((t (:background "gray10" :foreground "gray60" :inverse-video nil :box nil))))
+ '(mode-line ((t (:background "gray10" :foreground "gray60" :inverse-video nil :box nil))))
  )
