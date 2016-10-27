a882f941de9dca04a424f01937683b80e0e40024
Jeff Palmer <jeff@circleci.com>
2016-10-26 22:51:17 -0700

    magit full screen and window restore
    From http://whattheemacsd.com

--- init.el
+++ init.el
@@ -108,7 +108,19 @@
               (idle-highlight-mode t))))
 (use-package highlight-parentheses)
 (use-package magit
-  :bind ("C-c g" . magit-status))
+  :bind ("C-c g" . magit-status)
+  :config
+  (defadvice magit-status (around magit-fullscreen activate)
+    (window-configuration-to-register :magit-fullscreen)
+    ad-do-it
+    (delete-other-windows))
+  (defun magit-quit-session ()
+    "Restores the previous window configuration and kills the magit buffer"
+    (interactive)
+    (kill-buffer)
+    (jump-to-register :magit-fullscreen))
+  (define-key magit-status-mode-map (kbd "q") 'magit-quit-session))
+
 (use-package markdown-mode
   :config
   (add-hook 'markdown-mode-hook 'turn-on-orgtbl))
